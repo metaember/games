c15a3a82509f6e472cb3388ba8992ca91fbb8e53
Charles Bine <charles.bine@gmail.com>
2017-03-26 16:10:27 -0400

    this still has tons of bugs

--- 2048/2048.py
+++ 2048/2048.py
@@ -1,8 +1,34 @@
-from random import choice
+from random import randint
 import os
 import curses
 import numpy as np
 
+
+def get_random (table):
+    """ THIs has a bug"""
+    # count empty slots
+    tab = table.flatten()
+    count = 0
+    for item in tab:
+        if item == 0:
+            count += 1
+
+    # choose one at random
+    selected = randint(0,count-1)
+
+    # fill it with at random 1 or 2
+    count = 0
+    index = None
+    for idx, item in enumerate(tab):
+        if item == 0:
+            count += 1
+            if count == selected:
+                index = idx
+                break
+
+    tab[index] = randint(1,2)
+    ans = np.reshape(tab, (4,4))
+    return ans
 
 def table_to_row (table, direction):
     if direction == curses.KEY_UP:
@@ -10,7 +36,7 @@ def table_to_row (table, direction):
 
 def move(table, direction):
     """ Returns a table after the move has been completed"""
-
+    return table
 
 
     # Get the columns / rows in the right direction
@@ -23,7 +49,13 @@ def move(table, direction):
 def disp(table, win):
     for y in range (1,5):
         for x in range(1,5):
-
+            char = table[x-1, y-1]
+            if char == 0:
+                char = " "
+            else:
+                char = str(char)
+            win.addstr(y,x,char)
+    win.refresh()
 
 
 
@@ -39,17 +71,21 @@ def main (stdscr):
     game_win.refresh()
 
     stillPlaying = True
-    letter = ''
+
     table = np.array([[0]*4]*4)
+    turn = 0
     # make robust
     while stillPlaying:
-        disp(table, win)
+        disp(table, game_win)
+        letter = ''
         while letter not in (curses.KEY_UP, curses.KEY_DOWN, curses.KEY_LEFT, curses.KEY_RIGHT):
-            c = stdscr.getch()
-            if c == ord('q'):
+            letter = stdscr.getch()
+            if letter == ord('q'):
                 stillPlaying = False
                 return
-        table = move(table, c)
+        table = move(table, letter)
+        table = get_random(table)
+        turn += 1
 
 
 
